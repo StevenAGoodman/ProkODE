f5f4f9a53c3c1c7a605601c7c40fa1e601e7205d
Steven A. Goodman <163490937+StevenAGoodman@users.noreply.github.com>
2024-09-10 16:07:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 import sys
 sys.path.insert(1, '/workspaces/PROKODE-DOCKER/src')
-sys.path.insert(2, '/workspaces/PROKODE-DOCKER/src/processing')
+sys.path.insert(2, '/workspaces/PROKODE-DOCKER/src/preprocessing')
 from diffeq_plot import *
 from config_promo import *
-from get_tfbs_decay import *
+from preprocessing import *
+
+create_network(genome_loc, annotation_loc):
+  config_promo()
+  
+
+user_interface():
