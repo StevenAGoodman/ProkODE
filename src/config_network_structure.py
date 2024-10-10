6710be972ab944e6b231fd56ea942c321328e3a4
Steven A. Goodman <163490937+StevenAGoodman@users.noreply.github.com>
2024-10-09 21:59:59 -0400

    Update config_network_structure.py

--- src/config_network_structure.py
+++ src/config_network_structure.py
@@ -1,8 +1,11 @@
 import json
 import ast
 import re
+import math
+import linecache
 import numpy as np
 import pandas as pd
+from Bio.Seq import Seq
 from maths import *
 
 # network.json formatting:
@@ -26,10 +29,68 @@ from maths import *
 #     ...
 # }
 
-def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floating_genes):
+n_end_rule = {
+    "R": 2 * 60,
+    "K": 2 * 60,
+    "F": 2 * 60,
+    "L": 2 * 60,
+    "W": 2 * 60,
+    "Y": 2 * 60,
+    "H": 10 * 3600,
+    "I": 10 * 3600,
+    "D": 10 * 3600,
+    "E": 10 * 3600,
+    "N": 10 * 3600,
+    "Q": 10 * 3600,
+    "C": 10 * 3600,
+    "A": 10 * 3600,
+    "S": 10 * 3600,
+    "T": 10 * 3600,
+    "G": 10 * 3600,
+    "V": 10 * 3600,
+    "P": 10 * 3600,
+    "M": 10 * 3600    
+}
+
+def get_N_end(gene, annotation_df, genome_loc):
+    end_loc = int(annotation_df.loc[annotation_df['geneid']==gene, "end"])
+    print(end_loc)
+    line = linecache.getline(genome_loc, 1 + math.floor(end_loc / 81)).replace('\n','')
+    print(line)
+    rem = end_loc % 81
+    print(rem)
+    if rem > 3:
+        n_residue = Seq(line[rem - 3: rem])
+        print(n_residue)
+        n_residue = str(n_residue.translate())
+    else:
+        n_residue = linecache.getline(genome_loc, math.floor(end_loc / 81)).replace('\n','')[-3 + rem:] + line[:rem]
+        print(n_residue)
+        n_residue = str(Seq(n_residue).translate())
+    
+    if n_residue == "*":
+        end_loc
+        if rem > 3:
+            n_residue = Seq(line[rem - 3: rem])
+            print(n_residue)
+            n_residue = str(n_residue.translate())
+        else:
+            n_residue = linecache.getline(genome_loc, math.floor(end_loc / 81)).replace('\n','')[-3 + rem:] + line[:rem]
+            print(n_residue)
+            n_residue = str(Seq(n_residue).translate())
+
+        
+
+    return n_end_rule[n_residue]
+
+def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, genome_loc, floating_genes):
     # json start brackets
     output = {}
     gene_key = []
+
+    mrna_decay_prots = {"rne":0, "rnc":0, "rbn":0, "rng":0} # binding afinities to mrna
+    prot_decay_prots = {"clpX":0, "clpP":0, "lon":0} # binding affinities to proteins
+    # prot_decay_prots = {"clpX":{"kd":-, "targets":["",""]},}
 
     # loop over every gene
     for i, _ in operons_df.iterrows():
@@ -45,12 +106,22 @@ def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floati
                 mRNA_len = int(annotation_df.loc[annotation_df['geneid']==gene, 'end'].tolist()[0]) - int(annotation_df.loc[annotation_df['geneid']==gene, 'start'].tolist()[0])
 
                 # gene decays
-                mRNA_decay_arr = None
-                protein_decay_arr = None
+                mRNA_decay_arr = {}
+                for i in range(len(mrna_decay_prots)):
+                    pair = list(mrna_decay_prots.items())[i]
+                    if pair[0] in list(annotation_df["geneid"]):
+                        mRNA_decay_arr[pair[0]] = pair[1]
+                protein_decay_arr = {}
+                for i in range(len(prot_decay_prots)):
+                    pair = list(prot_decay_prots.items())[i]
+                    if pair[0] in list(annotation_df["geneid"]):
+                        protein_decay_arr[pair[0]] = pair[1]
+
+                n_end_rule = get_N_end(gene, annotation_df, genome_loc)
 
                 # within each gene's brackets:
                 syn = annotation_df.loc[annotation_df['geneid']==gene,'synonyms'].tolist()[0]
-                arr = {"synonyms":ast.literal_eval(syn),"transcript length":transcript_len,"mRNA length":mRNA_len, "mRNA decay": mRNA_decay_arr, "protein decay": protein_decay_arr}
+                arr = {"synonyms":ast.literal_eval(syn),"transcript length":transcript_len,"mRNA length":mRNA_len, "mRNA decay": mRNA_decay_arr, "protein decay": protein_decay_arr, "N end rule": n_end_rule}
                 reg_arr = {"polymerase":8.1}
                 arr["regulators"] = reg_arr
 
@@ -66,8 +137,16 @@ def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floati
             transcript_len = mRNA_len
 
             # gene decays
-            mRNA_decay_arr = None
-            protein_decay_arr = None
+            mRNA_decay_arr = {}
+            for i in len(mrna_decay_prots):
+                pair = list(mrna_decay_prots.items())[i]
+                if pair[0] in list(annotation_df["geneid"]):
+                    mRNA_decay_arr[pair[0]] = pair[1]
+            protein_decay_arr = {}
+            for i in range(len(prot_decay_prots)):
+                pair = list(prot_decay_prots.items())[i]
+                if pair[0] in list(annotation_df["geneid"]):
+                    protein_decay_arr[pair[0]] = pair[1]
 
             # within each gene's brackets:
             syn = annotation_df.loc[annotation_df['geneid']==gene,'synonyms'].tolist()[0]
@@ -88,6 +167,10 @@ def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floati
                 continue
 
             line = line.split(',')
+            if len(line) < 2:
+                print("line err:", line)
+                continue
+
             try:
                 operon = int(line[1])
                 operon_genes:str = operons_df.loc[operons_df['operonid']==operon, 'geneids'].tolist()[0]
@@ -123,48 +206,8 @@ def create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floati
 
     return network_loc, gene_key
 
-# def create_network_json(prokode_dir, gene_arr, decay_loc, tfbs_loc, annotation_df):
-#     # read file dependencies:
-#     decay_df = pd.read_csv(decay_loc) # names=['gene', 'decay_rate']
 
-#     # read tfbs.csv and append to network.json
-#     print('\t\twriting to network.json')
-#     network_loc = prokode_dir + '/src/network.json'
-#     with open(tfbs_loc, 'r') as tfbs_file: # names=[,'tf','tg','kd', 'beta]
-#         with open(network_loc, 'a') as network_file:
-#             # json start bracket
-#             network_file.write('{\n')
-
-#             # loop over every gene
-#             for tg in gene_arr:
-#                 # query decay rates file
-#                 tgdecay = decay_df.loc[decay_df['gene']==tg, 'decay_rate']
-
-#                 # within each gene's brackets:
-#                 syn = annotation_df.loc[annotation_df['geneid']==tg,'synonyms'].tolist[0]
-#                 arr = {"tg":tg, "synonyms":ast.literal_eval(syn), "tg_decay":tgdecay.to_list()[0]}
-#                 reg_arr = {}
-#                 # within each gene's "regulators": array
-#                 for _, tf_row in tfbs_df[tfbs_df['tg']==tg].iterrows():
-#                     tf_row['tf'] = "polymerase" if tf_row['tf'] == "rpoD" else tf_row['tf']
-#                     # beta = tf_row['beta']
-#                     kdtf = tf_row['Kd']
-#                     reg_arr[tf_row['tf']] = {"beta":"nan","kd_tf":kdtf}
-#                 arr["regulators"] = reg_arr
-
-#                 network_file.write(f'{json.loads(arr)},\n')
-
-#             # json end brackets
-#             network_file.write('}')
-
-#     return network_loc
-
-# def config_basal(Nns):
-#     Np = 6000
-#     Kdp =
-#     return Np / (Nns * Kdp)
-
-def network_main(prokode_dir, annotation_loc, operons_loc, tfbs_loc, floating_genes):
+def network_main(prokode_dir, genome_loc, annotation_loc, operons_loc, tfbs_loc, floating_genes):
     # configer basal / polymerase condition files
     # basal_loc =
 
@@ -173,7 +216,7 @@ def network_main(prokode_dir, annotation_loc, operons_loc, tfbs_loc, floating_ge
     operons_df = pd.read_csv(operons_loc, sep='\t')
 
     # create network.json
-    network_loc, gene_key = create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, floating_genes)
+    network_loc, gene_key = create_network_json(prokode_dir, tfbs_loc, annotation_df, operons_df, genome_loc, floating_genes)
 
     return network_loc, gene_key
 
