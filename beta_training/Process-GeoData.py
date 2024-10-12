c32a2be71e24f3288a52f9e3e7a9f1c3c5013c55
Caitlin Race <a7hp6zz@mmm.com>
2024-10-12 02:24:13 +0000

    changing paths

--- beta_training/Process-GeoData.py
+++ beta_training/Process-GeoData.py
@@ -3,10 +3,11 @@ import itertools
 import subprocess
 import random
 
-PYTHON_EXEC_PATH = "/usr/local/bin/python"
-PYSCRIPT_FILE_PATH = "/workspaces/ProkODE/beta_training/processing.py"
-PROKODE_BASEDIR =  "/workspaces/ProkODE" # !! NO end '/' !!
-DATA_FILE_PATH = "/workspaces/ProkODE/beta_training/GEO_expression_data/combined_gene_expression.csv"
+PYTHON_EXEC_PATH = "/onfs/homes/01/a7hp6zz/python/3.10.10-venv/bin/python"
+PROKODE_BASEDIR =  "/home/a7hp6zz/Repos/ProkODE" # !! NO end '/' !!
+PYSCRIPT_FILE_PATH = f"{PROKODE_BASEDIR}/beta_training/processing.py"
+DATA_FILE_PATH = f"{PROKODE_BASEDIR}/beta_training/GEO_expression_data/combined_gene_expression.csv"
+num_procs = 32
 
 chars_list = ["@AB", "@AB", "@A", "@AB", "@", "@", "@", "@A", "01", "01", "01", "01", "01", "01", "01", "01"]
 
@@ -22,7 +23,7 @@ def call_py(feature_str):
     print(feature_str)
     subprocess.run([PYTHON_EXEC_PATH, PYSCRIPT_FILE_PATH, feature_str, DATA_FILE_PATH, PROKODE_BASEDIR])
 
-p = Pool()
+p = Pool(num_procs)
 results = p.map(call_py, perms)
 
 p.close()
